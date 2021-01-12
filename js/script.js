bd150629d4c8048ffa9a9aad239f99dd67bad89d
dsa25 <s.dolya@globalmonitoring.ru>
2021-01-12 17:06:42 +0500

    12.01.21

--- js/script.js
+++ js/script.js
@@ -1,14 +1,52 @@
 function mainClock(){
-    var date = new Date();
-    $('#clock .clock__time').text(date.toLocaleString("ru", {hour: 'numeric',  minute: 'numeric',  second: 'numeric'}));
-    $('#clock .clock__date').text(date.toLocaleString("ru", {month: 'long',  day: 'numeric'}));
+	var date = new Date();
+	$('#clock .clock__time').text(date.toLocaleString("ru", {hour: 'numeric',  minute: 'numeric',  second: 'numeric', timeZone: 'Asia/Yekaterinburg'}));
+	$('#clock .clock__date').text(date.toLocaleString("ru", {month: 'long',  day: 'numeric'}));
 }
+
+
 
 var mainClockId = setInterval(mainClock, 1000);
 
 mainClock();
 
-// сохранить изменения группы
+
 $('body').on('click', '.menu-dashboard a', function() {
-	$(this).toggleClass('active');
+	var idboard = $(this).attr('data-idboard');
+	console.log(idboard);
+	if($(this).hasClass('active')){
+		$('.board[data-idboard='+idboard+']').removeClass('active');
+		if(idboard == 0){$('.board').removeClass('default');}
+		$(this).removeClass('active');
+	}else{
+		$('.board[data-idboard='+idboard+']').addClass('active');
+		if(idboard == 0){$('.board').addClass('default');}		
+		$(this).addClass('active');
+	}
+});
+
+
+$("#calendar-dashboard").datepicker({
+	monthNames: ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'],
+	dayNamesMin: ['Вс','Пн','Вт','Ср','Чт','Пт','Сб'],
+	firstDay: 1,
+	dateFormat:'dd.mm.y',
+	showOn: "button",
+	buttonImage: "/img/calendar_icon.svg",
+	buttonImageOnly: true,
+	buttonText: "Выбрать дату"
+});
+
+
+$( ".board" ).draggable({
+	containment: "html",
+	start: function(){
+		$('.board[data-idboard="0"]').removeClass('active');
+		$('.board').removeClass('default');
+	}
+});
+
+$('body').on('mousedown', '.ui-draggable:not(.current)', function() {
+	$('.ui-draggable').removeClass('current');
+	$(this).addClass('current');
 });
